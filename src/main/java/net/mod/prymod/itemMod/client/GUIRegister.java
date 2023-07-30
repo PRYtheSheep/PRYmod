dbbc3f69f1681a1e44b790f50b4fb530c50a66b4
PRYtheSheep <peyruoyang521@gmail.com>
2023-07-30 17:25:41 +0800

    Added simple GUI To-Do -> gui for generator, forge energy system for generator

---
+++ src/main/java/net/mod/prymod/itemMod/client/GUIRegister.java
@@ -0,0 +1,23 @@
+package net.mod.prymod.itemMod.client;
+
+import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.mod.prymod.ModBlock.PRYBlockContainer;
+import net.mod.prymod.PRYmod;
+
+public class GUIRegister {
+
+    public static final DeferredRegister<MenuType<?>> MENU_TYPES = DeferredRegister.create(ForgeRegistries.MENU_TYPES, PRYmod.MODID);
+
+    public static final RegistryObject<MenuType<PRYBlockContainer>> PRYBLOCK_CONTAINER = MENU_TYPES.register("pryblock",
+            () -> IForgeMenuType.create((windowId, inv, data) -> new PRYBlockContainer(windowId, inv.player, data.readBlockPos())));
+
+    public static void init(IEventBus modEventBus) {
+        MENU_TYPES.register(modEventBus);
+    }
+
+}
